55ef74b5817b2b56936f72b974f722c817f10008
abhibaluias <166683610+abhibaluias@users.noreply.github.com>
2025-07-17 10:26:19 +0530

    Update app.py

--- Flask-Project/app.py
+++ Flask-Project/app.py
@@ -1,23 +1,10 @@
-# Base image
-FROM python:3.8
+from flask import Flask
 
-# Set the working directory inside the container
-WORKDIR /app
+app = Flask(__name__)
 
-# Copy the requirements file
-COPY requirements.txt .
+@app.route('/')
+def hello():
+    return 'Hello, world!'
 
-# Install the project dependencies
-RUN pip install -r requirements.txt
-
-# Copy the application code into the container
-COPY . .
-
-# Expose the port the Flask application will be listening on
-EXPOSE 5000
-
-# Set environment variables, if necessary
-# ENV MY_ENV_VAR=value
-
-# Run the Flask application
-CMD ["python", "app.py"]
+if __name__ == '__main__':
+    app.run()
